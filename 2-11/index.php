a7267e4ce07e7e477b7047213f37fb0519741f07
kanekoshinya <knksny777@icloud.com>
2021-07-21 01:14:59 +0900

    2-11課題提出

---
+++ 2-11/index.php
@@ -0,0 +1,9 @@
+<form action="result.php" method="post">
+  名前：<input type="text" name="name" />
+  <br>
+  パスワード：<input type="password" name="password" />
+  <br>
+  メールアドレス：<input type="text" name="email" />
+  <br>
+  <input type="submit" value="送信" />
+</form>
---
+++ 2-11/result.php
@@ -0,0 +1,8 @@
+<?php
+$name = $_POST['name'];
+$password = $_POST['password'];
+$email = $_POST['email'];
+?>
+<p>私の名前は、<?php echo $name; ?></p>
+<p>私のパスワードは、<?php echo $password; ?></p>
+<p>私のメールアドレスは、<?php echo $email; ?></p>
