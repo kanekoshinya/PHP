556f229d152f548c2a9c083956d730f3120de6e6
kanekoshinya <knksny777@icloud.com>
2021-07-20 23:56:57 +0900

    2-7課題提出

---
+++ 2-7/index.php
@@ -0,0 +1,9 @@
+<?php
+$color = ["red" => "赤", "blue" => "青", "green" => "緑"];
+$color1 = ["red" => "赤", "blue" => "青", "green" => "緑", "yellow" => "黄色"];
+var_dump($color);
+echo '<br>';
+
+var_dump($color1);
+
+?>
