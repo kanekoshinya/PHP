ecd68953365b8125b3e4f9d6703d10eda381d0e5
kanekoshinya <knksny777@icloud.com>
2021-07-22 15:46:01 +0900

    2-7修正

--- 2-7/index.php
+++ 2-7/index.php
@@ -1,9 +1,10 @@
 <?php
 $color = ["red" => "赤", "blue" => "青", "green" => "緑"];
-$color1 = ["red" => "赤", "blue" => "青", "green" => "緑", "yellow" => "黄色"];
 var_dump($color);
+$color = ["red" => "赤", "blue" => "青", "green" => "緑", "yellow" => "黄色"];
+
 echo '<br>';
 
-var_dump($color1);
+var_dump($color);
 
 ?>
