3fad9bdf5aac2e547991071e3908517c6d950f0c
kanekoshinya <knksny777@icloud.com>
2021-07-28 00:43:45 +0900

    2-17課題修正

--- 2-17/index.php
+++ 2-17/index.php
@@ -1,29 +1,32 @@
 <?php
 $sum=0;
-for($n=1;$sum < 40;$n++){
+$n=1;
+while($sum < 40) {
   $number = mt_rand(1, 6);
   echo $n."回目＝".$number;
   echo '<br>';
+  $n++;
   $sum += $number;
   if($sum>=40){
-    echo "合計".$n."回でゴールしました";
+    echo "合計".($n - 1)."回でゴールしました";
   }
 }
+
 echo '<br>';
 echo '<br>';
 date_default_timezone_set( 'Asia/Tokyo' );
-
-if (strtotime(date('H:i')) > strtotime('6:00') && strtotime(date('H:i')) < strtotime('12:00'))
+$time = date("今H時台です", time());
+if (strtotime(date('H:i')) >= strtotime('6:00') && strtotime(date('H:i')) < strtotime('12:00'))
 {
-  echo date("今H時台です", time());
+  echo $time;
   echo '<br>';
   echo 'おはようございます！';
-}else if(strtotime(date('H:i')) > strtotime('12:00') && strtotime(date('H:i')) < strtotime('18:00')){
-  echo date("今H時台です", time());
+}else if(strtotime(date('H:i')) >= strtotime('12:00') && strtotime(date('H:i')) < strtotime('18:00')){
+  echo $time;
   echo '<br>';
   echo 'こんにちわ！';
 }else{
-  echo date("今H時台です", time());
+  echo $time;
   echo '<br>';
   echo 'こんばんわ！';
 }
